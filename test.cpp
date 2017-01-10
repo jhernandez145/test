327274f046b5f1e3d66f932dc59c198fc3ff7dc1
Jesus Hernandez <jesusahdz96@hotmail.com>
2017-01-10 08:33:11 -0600

    updated test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-	std::cout << "Hello World!";
+	std::cout << "Hello World!"<< std::endl;
+	std::cout << "This is a test." << std::endl;
 	return 0;
 }
